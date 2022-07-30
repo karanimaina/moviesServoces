f3e5f1940f6f57b58db0fca793dd7721b9070ccc
felixmaina <karani.maina2010@gmail.com>
2022-07-31 00:08:45 +0300

    add city class

---
+++ src/main/java/com/example/demo/City.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import java.sql.Timestamp;
+
+public class City {
+    private int id;
+    private int countryId;
+    private String city;
+    private Timestamp lastUpdate;
+}
