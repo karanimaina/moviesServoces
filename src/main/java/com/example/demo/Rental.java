fd5780303e3950572af6e8908deb80faddde1480
felixmaina <karani.maina2010@gmail.com>
2022-07-30 21:28:59 +0300

    add a rental clas "

---
+++ src/main/java/com/example/demo/Rental.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.sql.Timestamp;
+
+public class Rental {
+    private  int id;
+    private  int staffId;
+    private int customerId;
+    private int inventoryId;
+    private Timestamp rentalDate;
+    private Timestamp returnDate;
+    private Timestamp lastUpdate;
+}
