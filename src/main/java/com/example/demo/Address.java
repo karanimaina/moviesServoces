77bc854253aa4ff3159fab6b12432d854c5a0746
felixmaina <karani.maina2010@gmail.com>
2022-07-31 00:04:38 +0300

    add address class

---
+++ src/main/java/com/example/demo/Address.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.sql.Timestamp;
+
+public class Address {
+    private int id;
+    private int cityId;
+    private String address;
+    private String address2;
+    private int district;
+    private int postalCode;
+    private int phone;
+    private Timestamp lastUpdate;
+}
