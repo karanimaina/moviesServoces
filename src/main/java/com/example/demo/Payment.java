b4decff53d0836bc5719e1bdc2967792f59520e6
felixmaina <karani.maina2010@gmail.com>
2022-07-30 23:09:05 +0300

    add payment id class

---
+++ src/main/java/com/example/demo/Payment.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.sql.Timestamp;
+
+public class Payment {
+    private int id;
+    private int rentalId;
+    private  int customerId;
+    private int staffId;
+    private int amount;
+    private Timestamp paymentDate;
+
+}
