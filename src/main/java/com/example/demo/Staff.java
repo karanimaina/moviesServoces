bd67490e4fade471304c70ee68138083624533bb
felixmaina <karani.maina2010@gmail.com>
2022-07-30 23:51:57 +0300

    create a customer class

---
+++ src/main/java/com/example/demo/Staff.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.sql.Timestamp;
+
+public class Staff {
+    private int id;
+    private int storeId;
+    private int paymentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String active;
+    private String userName;
+    private String password;
+    private Timestamp lastUpdate;
+    private String pictureUrl;
+}
