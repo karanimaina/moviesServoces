fdb69f98175313efe37abad1241bdaaf1bc330d9
felixmaina <karani.maina2010@gmail.com>
2022-07-31 00:19:54 +0300

    generate the actor class

--- src/main/java/com/example/demo/Actor.java
+++ src/main/java/com/example/demo/Actor.java
@@ -7,4 +7,36 @@ public class Actor {
     private String firstName;
     private  String lastName;
     private Timestamp lastUpdate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
