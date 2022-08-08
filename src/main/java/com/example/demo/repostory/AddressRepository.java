7de9f07a2c72a7c0a63954c2d31b55c9375680cd
felixmaina <karani.maina2010@gmail.com>
2022-08-08 09:54:46 +0300

    set the actor model class

--- src/main/java/com/example/demo/repostory/AddressRepository.java
+++ src/main/java/com/example/demo/repostory/AddressRepository.java
@@ -1,8 +1,8 @@
-//package com.example.demo.repostory;
-//
-//import com.example.demo.model.Actor;
-//import com.example.demo.model.Address;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AddressRepository extends JpaRepository<Address, Integer>{
-//}
+package com.example.demo.repostory;
+
+import com.example.demo.model.Actor;
+import com.example.demo.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Integer>{
+}
